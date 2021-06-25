80ac78c2523bd180c89f835cc4545cbdd2064ab4
[dmswl27] <[dmswltpwns@naver.com]>
2021-06-25 15:30:04 +0900

    javascript practices

---
+++ javascript-practices/ex02.js
@@ -0,0 +1,21 @@
+/*
+변수와 데이터 타입
+
+[기본타입(primitive type)]
+1. number
+2. boolean
+3. string
+4. undefined
+
+[객체]
+*/
+
+var u;
+var i =10;
+var b =true;
+var s = "Hello World";
+
+console.log(u);
+console.log(i);
+console.log(b);
+console.log(s);
